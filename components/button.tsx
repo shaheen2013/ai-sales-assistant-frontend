5083200c24d6b2ba8ba85a263f47cda95ac60529
Sadman Sakib <ssakib4040@gmail.com>
2025-03-12 14:23:42 +0600

    Basic Structure & Theming created

---
+++ components/button.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames";
+import React from "react";
+
+interface buttonInterface {
+  children: React.ReactNode;
+  variant: "primary" | "secondary" | "outline-primary";
+  className?: string;
+
+  [key: string]: any;
+}
+
+export default function Button({
+  children,
+  variant,
+  className,
+  ...props
+}: buttonInterface) {
+  const clx = classNames(
+    "rounded-lg cursor-pointer py-[10px] px-6 hover:opacity-75 transition-opacity active:scale-95 transform duration-75 flex gap-2 items-center justify-center text-base font-semibold",
+    { "bg-primary-500 text-white": variant === "primary" },
+    {
+      "border-2 border-primary-500": variant === "outline-primary",
+    },
+    className
+  );
+
+  return (
+    <button className={clx} {...props}>
+      {children}
+    </button>
+  );
+}
