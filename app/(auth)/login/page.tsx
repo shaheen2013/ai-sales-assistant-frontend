955c01275ee289732651c7c313ab705b7736123c
Sadman Sakib <ssakib4040@gmail.com>
2025-04-16 18:04:12 +0600

    login in progress

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import SignupForm from "@/components/partials/dashboard/signup-form";
+
+export const metadata: Metadata = {
+  title: "Signup | AI Sales Assistant",
+};
+
+export default function SignUp() {
+  return <SignupForm />;
+}
--- components/shadcn/input.tsx
+++ components/shadcn/input.tsx
@@ -4,21 +4,41 @@ import { cn } from "@/lib/utils";
 
 interface InputProps extends React.ComponentProps<"input"> {
   error?: string;
+  preIcon?: React.ReactNode;
+  postIcon?: React.ReactNode;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, error, ...props }, ref) => {
+  ({ className, type, error, preIcon, postIcon, ...props }, ref) => {
     return (
       <div>
-        <input
-          type={type}
-          className={cn(
-            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-gray-100 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            className
+        <div className="relative flex items-center justify-center">
+          {preIcon && (
+            <div className="absolute left-2 top-1/2 -translate-y-1/2">
+              {preIcon}
+            </div>
           )}
-          ref={ref}
-          {...props}
-        />
+
+          <input
+            type={type}
+            className={cn(
+              "flex h-9 w-full rounded-md border border-[#D5D7DA] bg-transparent px-3 text-base  transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-gray-100 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+              {
+                "pl-10": preIcon,
+                "pr-10": postIcon,
+              },
+              className
+            )}
+            ref={ref}
+            {...props}
+          />
+
+          {postIcon && (
+            <div className="absolute right-2 top-1/2 -translate-y-1/2 bg-white">
+              {postIcon}
+            </div>
+          )}
+        </div>
 
         {error && <span className="text-red-500 text-sm mt-1">{error}</span>}
       </div>
