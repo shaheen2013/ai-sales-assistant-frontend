11efc4d55a7133a8539cc1eb20331fed64f22319
irfanhsajid <irfanhsajid@gmail.com>
2025-05-06 18:20:14 +0600

    test commit

--- app/dashboard/dealer/profile/page.tsx
+++ app/dashboard/dealer/profile/page.tsx
@@ -1,3 +1,8 @@
 export default function DashboardDealerProfile() {
-  return <div>Dashboard Dealer Profile</div>;
+  return (
+    <div>
+      Dashboard Dealer Profile
+      <p>::::: Dealer Profile :::::</p>
+    </div>
+  );
 }
