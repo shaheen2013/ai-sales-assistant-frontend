0537ce3a9bb16c9e546a14abdc5bdbe468a366bd
shaheen2013 <coredeveloper.2013@gmail.com>
2025-05-07 20:19:09 +0600

    feat: design talk appoint table.

--- app/dashboard/dealer/appointment/page.tsx
+++ app/dashboard/dealer/appointment/page.tsx
@@ -10,9 +10,8 @@ export default function DealerAppoinment() {
   return (
     <div className="grid grid-cols-12 gap-5">
       <div className="lg:col-span-8 col-span-12">
-        <h4 className="text-gray-400 text-xl font-semibold">Chat & Talk Appointment</h4>
-        <div className="text-gray-300 text-sm font-normal mt-1">Automatically generated customer list based on AI-driven chats and calls for seamless follow-ups.</div>
-
+  
+        <TalkToHumanSection />
         <TalkToHumanSection />
       </div>
       <div className="lg:col-span-4 col-span-12">
