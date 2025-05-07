c9c948551340ebe7696585a92d66e32e1566fcc0
shaheen2013 <coredeveloper.2013@gmail.com>
2025-05-07 19:02:51 +0600

    feat: Design talk to human table and Badge component.

--- app/dashboard/dealer/appointment/page.tsx
+++ app/dashboard/dealer/appointment/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TalkToHumanSection from "./_partials/talk-to-human/TalkToHumanSection";
 
 export const metadata = {
   title: "Team Management | Teez",
@@ -6,5 +7,17 @@ export const metadata = {
 };
 
 export default function DealerAppoinment() {
-  return <div>DealerAppoinment</div>;
+  return (
+    <div className="grid grid-cols-12 gap-5">
+      <div className="lg:col-span-8 col-span-12">
+        <h4 className="text-gray-400 text-xl font-semibold">Chat & Talk Appointment</h4>
+        <div className="text-gray-300 text-sm font-normal mt-1">Automatically generated customer list based on AI-driven chats and calls for seamless follow-ups.</div>
+
+        <TalkToHumanSection />
+      </div>
+      <div className="lg:col-span-4 col-span-12">
+
+      </div>
+    </div>
+  );
 }
