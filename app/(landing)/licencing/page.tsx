b2f6153a9b13cdf6e6578dbec7a34e985cf88b82
shaheen2013 <coredeveloper.2013@gmail.com>
2025-06-18 12:04:37 +0600

    feat: design licensing and usage rights page for mobile and desktop version.

--- app/(landing)/licencing/page.tsx
+++ app/(landing)/licencing/page.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import TermPrivacyLicenseBanner from '@/components/partials/termsPrivacyLicense/TermPrivacyLicenseBanner'
+import TermPrivacyLicenseBody from '@/components/partials/termsPrivacyLicense/TermPrivacyLicenseBody'
+import ProSeller from '@/components/pro-seller/ProSeller'
+import { licenseConstData } from '@/static/licenseConstData'
+import React from 'react'
 
-export default function page() {
-  return (
-    <div className="h-screen flex items-center justify-center text-xl font-bold">
-      Licencing page coming soon
-    </div>
-  );
+
+const page = () => {
+    return (
+        <div>
+            <TermPrivacyLicenseBanner
+                title="Licensing & Usage Rights"
+                subtitle="Learn about your rights to use the Teez platform, including license terms, usage restrictions, and integration policies. This section outlines what you're allowed to do with the app, how your subscription governs access, and how we protect our intellectual property."
+            />
+            <TermPrivacyLicenseBody
+                title='Licensing & Rights'
+                items={licenseConstData}
+            />
+            <ProSeller />
+        </div>
+    )
 }
+
+export default page
