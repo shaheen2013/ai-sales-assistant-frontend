0bec3f1c96b9c72fc51fe5b2307d4fd234f7badf
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2025-07-03 11:36:18 +0600

    slider added

--- app/(auth)/(dealer)/layout.tsx
+++ app/(auth)/(dealer)/layout.tsx
@@ -1,24 +1,19 @@
 "use client";
 
 import React from "react";
-import { usePathname } from "next/navigation";
+import { EmblaOptionsType } from "embla-carousel";
 
 import AuthHeader from "@/components/AuthHeader";
-
-import Image from "next/image";
+import LayoutCarousel from "./layoutCarousel/layoutCarousel";
 
 export default function GeneralLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
-  const photoMap: Record<string, string> = {
-    "/general/signup": "/images/general-signup.svg",
-    "/general/login": "/images/general-login.svg",
-    "/": "/images/home.jpg",
-  };
+  const OPTIONS: EmblaOptionsType = {};
+  const SLIDE_COUNT = 5;
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
   return (
     <div className="w-full lg:grid grid-cols-12 h-screen">
@@ -53,15 +48,17 @@ export default function GeneralLayout({
 
       {/* right temp */}
       <div className="lg:col-span-6 col-span-12 p-4 hidden lg:block fixed right-0 top-0 h-screen w-full lg:w-1/2">
-        <div className=" h-full rounded-lg relative">
+        <div className=" h-full rounded-lg relative bg-[#F3F4F5] dev">
           {/* right content */}
-          <Image
+          {/* <Image
             src={photoMap[pathname] || "/images/general-signup.svg"}
             alt="auth"
             fill
             className="rounded-lg absolute top-0 bottom-0 left-0 right-0 m-auto object-cover"
             priority={true}
-          />
+          /> */}
+
+          <LayoutCarousel slides={SLIDES} options={OPTIONS} />
         </div>
       </div>
     </div>
